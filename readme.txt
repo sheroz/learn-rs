2833447bf3dba055a3a3a07dfbef1998d3db5e25
sheroz.khaydarov <sheroz.khaydarov@gmail.com>
2023-05-12 10:23:31 +0300

    more vector ops

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 Rust Book: https://www.cs.brandeis.edu/~cs146a/rust/doc-02-21-2015/book/README.html
 
+Rust Collections: https://doc.rust-lang.org/std/collections/index.html
+
--- src/my_collections/my_vector.rs
+++ src/my_collections/my_vector.rs
@@ -1,25 +1,39 @@
 pub fn vector1(){
-    println!("\nVectors:");
+    println!("\n***\nVectors:");
 
     let mut vec1 = vec![1, 2, 3];
-    print_my_vector(&vec1);
+    print_my_vector("Sample vector", &vec1);
 
-    vec1.push(41);
-    vec1.push(42);
+    vec1.push(4);
+    vec1.push(5);
+    assert_eq!(vec1, vec![1,2,3,4,5]);
+    print_my_vector("After Push(4), Push(5)", &vec1);
 
-    print_my_vector(&vec1);
+    vec1.pop();
+    assert_eq!(vec1, vec![1,2,3,4]);
+    print_my_vector("After Pop()", &vec1);
 
     for item in vec1.iter_mut() {
         *item += 10;
     }
+    assert_eq!(vec1, vec![11,12,13,14]);
+    print_my_vector("After +10", &vec1);
 
-    print_my_vector(&vec1);
+    vec1.remove(2);
+    assert_eq!(vec1, vec![11,12,14]);
+    print_my_vector("After remove(2)",&vec1);
+
+    vec1.insert(1,77);
+    assert_eq!(vec1, vec![11,77,12,14]);
+    print_my_vector("After insert(1,77)",&vec1);
+
 }
 
-fn print_my_vector(myvector: &Vec<i32>) {
-    println!("\nsize: {}", myvector.len());
+fn print_my_vector(msg: &str, myvector: &Vec<i32>) {
+    println!("{}", msg);
+    println!("size: {}", myvector.len());
     for item in myvector.iter() {
         print!("{} " , item);
     }
-    println!("");
+    println!("\n");
 }
