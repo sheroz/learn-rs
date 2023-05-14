9c6252f6cff7f3715df1fad7d376abb59f0c386b
sheroz.khaydarov <sheroz.khaydarov@gmail.com>
2023-05-14 16:10:44 +0300

    added multi-function bench sample

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,8 +1,22 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use my_rust::fibonacci;
 
-pub fn fibonacci_benchmark(c: &mut Criterion) {
-    c.bench_function("fibonacci(20)", |b| b.iter(|| my_rust::fibonacci::fibonacci(black_box(20))));
+fn fibonacci_benchmark(c: &mut Criterion) {
+    c.bench_function("fibonacci_recursive(20)", |b| b.iter(|| fibonacci::fibonacci_recursive(black_box(20))));
+    c.bench_function("fibonacci_iterative(20)", |b| b.iter(|| fibonacci::fibonacci_iterative(black_box(20))));
 }
 
-criterion_group!(benches, fibonacci_benchmark);
+
+fn fibonacci_multiple_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Fibonacci");
+    for i in [20, 21].iter() {
+        group.bench_with_input(BenchmarkId::new("fibonacci_recursive", i), i, 
+            |b, i| b.iter(|| fibonacci::fibonacci_recursive(*i)));
+        group.bench_with_input(BenchmarkId::new("fibonacci_iterative", i), i, 
+            |b, i| b.iter(|| fibonacci::fibonacci_iterative(*i)));
+    }
+    group.finish();
+}
+
+criterion_group!(benches, fibonacci_benchmark, fibonacci_multiple_benchmark);
 criterion_main!(benches);
--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,21 +1,44 @@
-pub fn fibonacci(number: u32) -> u32 {
-    if number <= 1 {
-        return number;
+
+#[inline]
+pub fn fibonacci_recursive(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
     }
 
-    fibonacci(number - 1) + fibonacci(number - 2)
+    fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+}
+
+#[inline]
+pub fn fibonacci_iterative(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+
+    let mut a = 0;
+    let mut b = 1;    
+    for _ in 1..n {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    b
 }
 
 #[cfg(test)]
 mod tests {
-    use super::fibonacci;
     #[test]
-    fn fibonacci_test() {
+    fn fibonacci_recursive_test() {
         let s = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
-        let r: Vec<_> = (0..s.len()).map(|n| fibonacci(n as u32)).collect();
+        let r: Vec<_> = (0..s.len()).map(|n| super::fibonacci_recursive(n as u32)).collect();
         assert_eq!(&s[..], &r[..]);
     }
 
+    #[test]
+    fn fibonacci_iterative_test() {
+        let s = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+        let r: Vec<_> = (0..s.len()).map(|n| super::fibonacci_iterative(n as u32)).collect();
+        assert_eq!(&s[..], &r[..]);
+    }
     /*
     #[bench]
     fn bench_fibonacci_classic(b: &mut Bencher) {
