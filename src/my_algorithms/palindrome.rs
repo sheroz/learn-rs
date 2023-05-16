8fba21c935d616adc203318ab46bbc848d15123f
sheroz.khaydarov <sheroz.khaydarov@gmail.com>
2023-05-16 16:08:29 +0300

    added palindrome check without using regex

--- src/my_algorithms/palindrome.rs
+++ src/my_algorithms/palindrome.rs
@@ -3,7 +3,10 @@ extern crate unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn is_palindrome(sample: &str) -> bool {
+    is_palindrome_raw(sample)
+}
 
+pub fn is_palindrome_regex(sample: &str) -> bool {
     // make all characters lower case
     let txt = sample.to_string().to_lowercase();
 
@@ -21,18 +24,72 @@ pub fn is_palindrome(sample: &str) -> bool {
     clean_txt == reverse_txt
 }
 
+pub fn is_palindrome_raw(sample: &str) -> bool {
+    if sample.len() == 0 {
+        return false;
+    }
+
+    // make all characters lower case
+    let txt = sample.to_string().to_lowercase();
+    let mut iterator = txt.graphemes(true).into_iter();
+    let mut option_char_start: Option<&str>;
+    let mut option_char_end: Option<&str>;
+
+    let exclude_pattern = [' ', '.', ',', '!', ':', ';', '\'', '’', '"', '-'];
+    let mut char_start: &str = "";
+    let mut char_end: &str = "";
+
+    loop {
+        
+        loop {
+            option_char_start = iterator.next();
+            if option_char_start.is_none() {
+                break;
+            }
+
+            char_start = option_char_start.unwrap();
+            if char_start.find(&exclude_pattern).is_none() {
+                break;
+            }
+        }
+
+        loop {
+            option_char_end = iterator.next_back();
+            if option_char_end.is_none() {
+                break;
+            }
+
+            char_end = option_char_end.unwrap();
+            if char_end.find(&exclude_pattern).is_none() {
+                break;
+            }
+        }
+
+        if option_char_start.is_none() || option_char_end.is_none() {
+            break;
+        }
+
+        if char_start != char_end {
+            return false;
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn palindrome_test() {
-
         // palindome samples
         assert!(super::is_palindrome("A man, a plan, a canal: Panama!"));
         assert!(super::is_palindrome("Dammit I’m mad."));
         assert!(super::is_palindrome("Engage le jeu que je le gagne."));
         assert!(super::is_palindrome("I topi non avevano nipoti."));
         assert!(super::is_palindrome("Autore, ero tua."));
-        assert!(super::is_palindrome("Socorram-me subi no onibus em Marrocos."));
+        assert!(super::is_palindrome(
+            "Socorram-me subi no onibus em Marrocos."
+        ));
         assert!(super::is_palindrome("A mala nada na lama."));
         assert!(super::is_palindrome("А тот суп – пустота."));
         assert!(super::is_palindrome("Нажал кабан на баклажан."));
@@ -40,7 +97,9 @@ mod tests {
         // non-palindome samples
         assert!(!super::is_palindrome("Lorem ipsum dolor sit amet,"));
         assert!(!super::is_palindrome("consectetur adipiscing elit,"));
-        assert!(!super::is_palindrome("sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+        assert!(!super::is_palindrome(
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        ));
         assert!(!super::is_palindrome("A plan, a man, a canal: Panama!"));
         assert!(!super::is_palindrome("Нажал баклажан на кабан."));
     }
