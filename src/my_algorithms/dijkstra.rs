02930da23660bbf519c1106b93163aa7896c7e19
sheroz.khaydarov <sheroz.khaydarov@gmail.com>
2023-05-20 15:00:48 +0300

    dijkstra: refactored

--- src/my_algorithms/dijkstra.rs
+++ src/my_algorithms/dijkstra.rs
@@ -145,18 +145,23 @@ pub fn shortest_path_tree_matrix(graph_matrix: &Vec<Vec<u32>>, source: usize) ->
 
         let column = &graph_matrix[visiting_index];
 
+        // applying the core dijkstra algorithm
+        // calculating distance from and previous node 
         for index in 0..node_count {
-            if column[index] > 0 {
-                let mut record = &mut shortest_path_tree[index];
-                let new_distance = column[index] + node_distance;
-                if !visited[index] && record.distance > new_distance {
-                        record.distance = new_distance;
-                        record.previous = visiting_index;
+            if !visited[index] {
+                let distance = column[index];
+                if  distance > 0 {
+                    let mut record = &mut shortest_path_tree[index];
+                    let new_distance = distance + node_distance;
+                    if record.distance > new_distance {
+                            record.distance = new_distance;
+                            record.previous = visiting_index;
+                    }
                 }
             }
         }
 
-        // check for min distance
+        // checking for not visited record with min distance
         visiting_index = usize::MAX;
         let mut min_distance = u32::MAX;
         for index in 0..node_count {
