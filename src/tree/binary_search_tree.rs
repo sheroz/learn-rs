7bbf8f42056985190a9d4cb5590fdb13b4d004cb
sheroz <sheroz@dreamlogic.io>
2023-08-26 22:48:52 +0300

    WIP: sample binary search tree

--- src/tree/binary_search_tree.rs
+++ src/tree/binary_search_tree.rs
@@ -1,8 +1,18 @@
 // https://www.programiz.com/dsa/binary-search-tree
 
-use std::collections::{HashSet, VecDeque};
+/*
+A binary search tree (BST) is a node-based binary tree data structure that has the following properties.
+
+1. The left subtree of a node contains only nodes with keys less than the node’s key.
+2. The right subtree of a node contains only nodes with keys greater than the node’s key.
+3. Both the left and right subtrees must also be binary search trees.
+4. Each node (item in the tree) has a distinct key.
+
+https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
+*/
 
 use crate::tree::binary_tree::*;
+use std::collections::{HashSet, VecDeque};
 
 pub struct BinarySearchTree {
     pub tree: BinaryTree,
@@ -13,6 +23,41 @@ impl BinarySearchTree {
         BinarySearchTree { tree }
     }
 
+    pub fn is_binary_search_tree3(node: BinaryTreeNodeRef) -> bool {
+        let mut queue = VecDeque::new();
+
+        let mut prev = 0;
+        let mut prev_valid = false;
+
+        let leftmost = BinaryTree::leftmost(node);
+        queue.push_back(leftmost);
+
+        while let Some(node_ref) = queue.pop_front() {
+            let node = node_ref.borrow();
+
+            let mut cur = node.data;
+            if prev_valid && cur <= prev {
+                return false;
+            }
+            prev = cur;
+            prev_valid = true;
+
+            if let Some(parent) = node.parent.as_ref() {
+                cur = parent.borrow().data;
+                if cur <= prev {
+                    return false;
+                }
+                prev = cur;
+            }
+
+            if let Some(right) = node.right.as_ref() {
+                let leftmost = BinaryTree::leftmost(right.clone());
+                queue.push_back(leftmost);
+            }
+        }
+        true
+    }
+
     pub fn is_binary_search_tree2(node: BinaryTreeNodeRef) -> bool {
         let flatten = BinaryTree::flatten_left_to_right(node);
         let values: Vec<_> = flatten.iter().map(|n| n.borrow().data).collect();
@@ -20,19 +65,7 @@ impl BinarySearchTree {
     }
 
     pub fn is_binary_search_tree1(node: BinaryTreeNodeRef) -> bool {
-        /*
-        A binary search tree (BST) is a node-based binary tree data structure that has the following properties.
-
-        1. The left subtree of a node contains only nodes with keys less than the node’s key.
-        2. The right subtree of a node contains only nodes with keys greater than the node’s key.
-        3. Both the left and right subtrees must also be binary search trees.
-        4. Each node (item in the tree) has a distinct key.
-
-        https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
-        */
-
         let mut keys = HashSet::<u32>::new();
-
         let mut queue = VecDeque::new();
         queue.push_back(node.clone());
         while let Some(node) = queue.pop_front() {
@@ -97,5 +130,17 @@ mod tests {
     fn non_binary_search_tree2() {
         let root = populate_balanced_binary_tree();
         assert!(!BinarySearchTree::is_binary_search_tree2(root));
+    }
+
+    #[test]
+    fn is_binary_search_tree3() {
+        let root = populate_balanced_binary_search_tree();
+        assert!(BinarySearchTree::is_binary_search_tree3(root));
+    }
+
+    #[test]
+    fn non_binary_search_tree3() {
+        let root = populate_balanced_binary_tree();
+        assert!(!BinarySearchTree::is_binary_search_tree3(root));
     }
 }
